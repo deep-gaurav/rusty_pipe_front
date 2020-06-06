7a9a61edf3b1f49eb8f6e4d43641bf865815cae3
Deep <deepgauravraj@gmail.com>
2020-06-07 00:57:28 +0530

    cache rust

--- netlify.sh
+++ netlify.sh
@@ -2,6 +2,14 @@
 
 set -e
 
+shopt -s dotglob
+
+mkdir -p node_modules/rustcache
+mkdir target
+
+mv node_modules/rustcache/* target
+
+
 curl https://sh.rustup.rs -sSf | sh -s - --default-toolchain stable -y
 source ~/.cargo/env
 
@@ -10,3 +18,6 @@ cargo install wasm-pack
 npm run build
 
 ls -l dist/
+
+mkdir -p node_modules/rustcache
+mv target/* node_modules/rustcache
