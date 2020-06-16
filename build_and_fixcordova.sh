b99c326c2ed1828a3edc1436a72b01208a3e8c86
Deep <deepgauravraj@gmail.com>
2020-06-16 20:01:16 +0530

    fix deploys

--- build_and_fixcordova.sh
+++ build_and_fixcordova.sh
@@ -15,4 +15,10 @@ cp ../dist/* www/
 rm www/index.html
 cp fixes/index.html www/index.html
 cordova build electron --release --verbose
+cd ..
+mkdir deploys
+cp cordovawrap/platforms/electron/build/*.exe deploys/
+cp cordovawrap/platforms/electron/build/*.AppImage deploys/
+ls deploys/
+
 # ls -R
