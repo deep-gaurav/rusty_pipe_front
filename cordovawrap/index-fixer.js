913ee47d4c830a3952a6b024161c786dbe26082a
Deep <deepgauravraj@gmail.com>
2020-06-17 02:21:01 +0530

    fix index

--- cordovawrap/index-fixer.js
+++ cordovawrap/index-fixer.js
@@ -6,9 +6,9 @@ let filename = 'www/index.html'
 let rawdata = fs.readFileSync(filename);
 let index = rawdata.toString();
 
-index.replace("/app","app");
-index.replace("/bulma","bulma");
-index.replace("/font","font");
+index = index.replace("/app","app");
+index = index.replace("/bulma","bulma");
+index = index.replace("/font","font");
 
 console.log(index)
  
