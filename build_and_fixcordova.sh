c2cc1d24c06377d163b83a3e3bdfee90696c55c9
Deep <deepgauravraj@gmail.com>
2020-06-16 15:39:06 +0530

    cordova fix

--- build_and_fixcordova.sh
+++ build_and_fixcordova.sh
@@ -1,9 +1,12 @@
 set -x
 cd cordovawrap
-pwd
+mkdir platforms
+mkdir plugins
+mkdir node_modules
+mkdir www
 cordova platform add electron --verbose
 rm www/*
-cp dist/* www/
+cp ../dist/* www/
 rm www/index.html
 cp fixes/index.html www/index.html
 cordova build electron --release
