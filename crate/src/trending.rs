8a70615c90b61c0f6bea0f6b7738f2cf03132d5a
Deep <deepgauravraj@gmail.com>
2020-06-17 21:28:59 +0530

    remove prebuild, add pagination in home

--- crate/src/trending.rs
+++ crate/src/trending.rs
@@ -10,10 +10,12 @@ pub struct Trending {
     link: ComponentLink<Self>,
     is_loading: bool,
     trending_extractor: Result<YTTrendingExtractor, ParsingError>,
+    page_num:u32
 }
 
 pub enum Msg {
     Loaded(Result<YTTrendingExtractor, ParsingError>),
+    LoadMore
 }
 
 impl Component for Trending {
@@ -30,6 +32,7 @@ impl Component for Trending {
             link,
             is_loading: true,
             trending_extractor: Err(ParsingError::from("still loading")),
+            page_num:1
         }
     }
 
@@ -42,6 +45,10 @@ impl Component for Trending {
             Msg::Loaded(ex) => {
                 self.trending_extractor = ex;
                 self.is_loading = false;
+                true
+            }
+            Msg::LoadMore => {
+                self.page_num+=1;
                 true
             }
         }
@@ -57,12 +64,32 @@ impl Component for Trending {
                 Ok(extractor) => match extractor.get_videos() {
                     Ok(extractor) => {
                         let mut results = vec![];
-                        for vid in extractor {
+                        for vid in extractor.iter().take((self.page_num*20) as usize) {
                             results.push(YTSearchItem::StreamInfoItem(vid.clone()));
                         }
                         let items = layout_result(320_f64, &results);
                         html! {
-                            for items
+                            <>
+                            {for items}
+                            {
+                                if extractor.len()>(self.page_num*20) as usize{
+                                    html!{
+                                        <button onclick=self.link.callback(|_|Msg::LoadMore) class={
+                                            let mut classes = "button is-fullwidth".to_string();
+                                            if self.is_loading {
+                                              classes = format!("{} is-loading",classes);
+                                            }
+                                            classes
+                                        }>{"Load More"}</button>
+                                    }
+                                }else{
+                                    log::info!("extractor len : {} pagel: {}",extractor.len(),self.page_num*20);
+                                    html!{
+
+                                    }
+                                }
+                            }
+                            </>
                         }
                     }
                     Err(err) => html! {
