389d2e497430e78fc6800af42d0d8b5f2d09221c
Deep <deepgauravraj@gmail.com>
2020-07-19 12:32:28 +0530

    download wasm-pack

--- netlify.sh
+++ netlify.sh
@@ -17,6 +17,7 @@ set -e
    curl https://sh.rustup.rs -sSf | sh -s - --default-toolchain stable -y
    source ~/.cargo/env
 
+   curl -L https://github.com/rustwasm/wasm-pack/releases/download/v0.9.1/wasm-pack-v0.9.1-x86_64-unknown-linux-musl.tar.gz --output wasm-pack-v0.9.1-x86_64-unknown-linux-musl.tar.gz
    ls -l
    tar -zxvf wasm-pack-v0.9.1-x86_64-unknown-linux-musl.tar.gz
    export PATH="$PATH:$PWD/wasm-pack-v0.9.1-x86_64-unknown-linux-musl"
